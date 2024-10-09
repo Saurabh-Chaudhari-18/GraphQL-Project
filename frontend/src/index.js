a65433fe088f8af4e686b22f1fd5b1efbda15523
Saurabh-Chaudhari-18 <saurabh100chaudhari@gmail.com>
2024-10-09 14:58:07 +0530

    frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://graphql-backend-1-geng.onrender.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
